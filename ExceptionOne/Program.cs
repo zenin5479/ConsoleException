57d7407433c4bc0c3a637492afce1e24af604748
zenin5479 <zenin5479@gmail.com>
2024-09-03 21:44:21 +0300

    Добавьте файлы проекта.

---
+++ ExceptionOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionOne
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
