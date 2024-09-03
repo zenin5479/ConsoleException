489e9b3a2971a8272446cc7b53e71b3966e0c2d9
zenin5479 <zenin5479@gmail.com>
2024-09-03 21:52:08 +0300

    Update 0.1 Обновлен метод

--- ExceptionOne/Program.cs
+++ ExceptionOne/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+// System.Exception
+
 namespace ExceptionOne
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
-         Console.WriteLine("Hello World!");
+         int[] nums = new int[4];
+
+         try
+         {
+            Console.WriteLine("Сгенерировать исключение в связи с выходом индекса за границы массива int[4] nums");
+            Console.WriteLine("До генерирования исключения.");
+
+            // Сгенерировать исключение в связи с выходом индекса за границы массива
+            for (int i = 0; i < 5; i++)
+            {
+               nums[i] = i;
+               Console.WriteLine("nums[(0)]: {0}", nums[i]);
+            }
+            Console.WriteLine("He подлежит выводу");
+         }
+         catch (IndexOutOfRangeException)
+         {
+            // Перехватить исключение.
+            Console.WriteLine("Индекс вышел за границы массива!");
+         }
+         Console.WriteLine("После блока перехвата исключения.");
+
+         Console.ReadKey();
       }
    }
 }
