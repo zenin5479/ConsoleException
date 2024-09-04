6fb685faa0a9094fbb50a99c2592abe05ad444e9
zenin5479 <zenin5479@gmail.com>
2024-09-04 21:38:08 +0300

    Update 2.3 Обновлен метод

--- ExceptionThree/Program.cs
+++ ExceptionThree/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
+// Предоставить исполняющей системе возможность самой обрабатывать ошибки
+// System.Exception
+
 namespace ExceptionThree
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
-         Console.WriteLine("Hello World!");
+         int[] nums = new int[4];
+         Console.WriteLine("До генерирования исключения");
+         // Сгенерировать исключение в связи с выходом индекса за границы массива
+         for (int i = 0; i < 10; i++)
+         {
+            nums[i] = i;
+            Console.WriteLine("nums[{0}]: {1}", i, nums[i]);
+         }
       }
    }
 }
