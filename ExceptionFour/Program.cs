bc8af7b2d3f7944dc3a808588765374fb9bd0504
zenin5479 <zenin5479@gmail.com>
2024-09-05 21:18:34 +0300

    Update 2.9 Обновлен метод

--- ExceptionFour/Program.cs
+++ ExceptionFour/Program.cs
@@ -7,9 +7,23 @@ namespace ExceptionFour
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
-         Console.WriteLine("Hello World!");
+         int[] numer = { 4, 8, 16, 32, 64, 128 };
+         int[] denom = { 2, 0, 4, 4, 0, 8 };
+         for (int i = 0; i < numer.Length; i++)
+         {
+            try
+            {
+               Console.WriteLine(numer[i] + " / " +
+                                 denom[i] + " равно " + numer[i] / denom[i]);
+            }
+            catch (DivideByZeroException)
+            {
+               // Перехватываем исключение.
+               Console.WriteLine("Делить на нуль нельзя!");
+            }
+         }
       }
    }
 }
