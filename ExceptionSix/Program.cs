1a12cc1396cbfc63671f40529950caded90fe9b6
zenin5479 <zenin5479@gmail.com>
2024-09-05 22:06:38 +0300

    Update 4.2 Обновлен метод

--- ExceptionSix/Program.cs
+++ ExceptionSix/Program.cs
@@ -9,21 +9,22 @@ namespace ExceptionSix
    {
       static void Main()
       {
-         // Здесь массив numer длиннее массива denom.
+         // Здесь массив divisible длиннее массива divider
          int[] divisible = { 4, 8, 16, 32, 64, 128, 256, 512, 1024 };
          int[] divider = { 2, 0, 4, 8, 0, 16, 32 };
          for (int i = 0; i < divisible.Length; i++)
          {
             try
             {
-               Console.WriteLine(divisible[i] + " / " + divider[i] + " равно " + divisible[i] / divider[i]);
+               Console.WriteLine(divisible[i] + "/" + divider[i] + " равно " + divisible[i] / divider[i]);
             }
             catch
             {
-               Console.WriteLine(
-                  "Произошло некоторое исключение");
+               Console.WriteLine("Произошло некоторое исключение");
             }
          }
+
+         Console.ReadKey();
       }
    }
 }
