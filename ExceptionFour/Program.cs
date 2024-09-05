e42489394b6f83c503e21d2b19a7bdd53e05526d
zenin5479 <zenin5479@gmail.com>
2024-09-05 21:21:00 +0300

    Update 3.0 Обновлен метод

--- ExceptionFour/Program.cs
+++ ExceptionFour/Program.cs
@@ -9,18 +9,17 @@ namespace ExceptionFour
    {
       static void Main()
       {
-         int[] numer = { 4, 8, 16, 32, 64, 128 };
+         int[] divisible = { 4, 8, 16, 32, 64, 128 };
          int[] denom = { 2, 0, 4, 4, 0, 8 };
-         for (int i = 0; i < numer.Length; i++)
+         for (int i = 0; i < divisible.Length; i++)
          {
             try
             {
-               Console.WriteLine(numer[i] + " / " +
-                                 denom[i] + " равно " + numer[i] / denom[i]);
+               Console.WriteLine(divisible[i] + " / " + denom[i] + " равно " + divisible[i] / denom[i]);
             }
             catch (DivideByZeroException)
             {
-               // Перехватываем исключение.
+               // Перехватываем исключение
                Console.WriteLine("Делить на нуль нельзя!");
             }
          }
