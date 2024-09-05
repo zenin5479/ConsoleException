7fdf8b2a72649fa8966338196424888858d8c8ce
zenin5479 <zenin5479@gmail.com>
2024-09-05 21:51:03 +0300

    Update 3.8 Обновлен метод

--- ExceptionSix/Program.cs
+++ ExceptionSix/Program.cs
@@ -7,7 +7,7 @@ namespace ExceptionSix
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
          Console.WriteLine("Hello World!");
       }
