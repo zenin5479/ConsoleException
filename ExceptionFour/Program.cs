f0b392b6913abfdc854ec137a181a288bcda630b
zenin5479 <zenin5479@gmail.com>
2024-09-05 21:22:14 +0300

    Update 3.1 Обновлен метод

--- ExceptionFour/Program.cs
+++ ExceptionFour/Program.cs
@@ -10,12 +10,12 @@ namespace ExceptionFour
       static void Main()
       {
          int[] divisible = { 4, 8, 16, 32, 64, 128 };
-         int[] denom = { 2, 0, 4, 4, 0, 8 };
+         int[] divider = { 2, 0, 4, 4, 0, 8 };
          for (int i = 0; i < divisible.Length; i++)
          {
             try
             {
-               Console.WriteLine(divisible[i] + " / " + denom[i] + " равно " + divisible[i] / denom[i]);
+               Console.WriteLine(divisible[i] + " / " + divider[i] + " равно " + divisible[i] / divider[i]);
             }
             catch (DivideByZeroException)
             {
