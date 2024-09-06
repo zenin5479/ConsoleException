1e9574bc2ab7b1a40be5d3a88834adb926f346b2
zenin5479 <zenin5479@gmail.com>
2024-09-06 22:14:53 +0300

    Update 4.6 Обновлен метод

--- ExceptionSeven/Program.cs
+++ ExceptionSeven/Program.cs
@@ -9,7 +9,33 @@ namespace ExceptionSeven
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+         int[] denom = { 2, 0, 4, 4, 0, 8 };
+         try
+         {
+            // Внешний try-блок.
+            for (int i = 0; i < numer.Length; i++)
+            {
+               try
+               {
+                  // Вложенный try-блок.
+                  Console.WriteLine(numer[i] + " / " +
+                                    denom[i] + " равно " + numer[i] / denom[i]);
+               }
+               catch (DivideByZeroException)
+               {
+                  // Перехватываем исключение.
+                  Console.WriteLine("На нуль делить нельзя!");
+               }
+            }
+         }
+         catch (IndexOutOfRangeException)
+         {
+            // Перехватываем исключение.
+            Console.WriteLine("Нет соответствующего элемента.");
+            Console.WriteLine(
+               "Неисправимая ошибка — программа завершена.");
+         }
 
          Console.ReadKey();
       }
