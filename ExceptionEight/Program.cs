1bd23bc9cc11b5ac77ecda340687c456d94b8512
zenin5479 <zenin5479@gmail.com>
2024-09-06 22:54:17 +0300

    Update 5.5 Обновлен метод

--- ExceptionEight/Program.cs
+++ ExceptionEight/Program.cs
@@ -11,15 +11,15 @@ namespace ExceptionEight
       {
          try
          {
-            Console.WriteLine("До генерирования исключения.");
+            Console.WriteLine("До генерирования исключения");
             throw new DivideByZeroException();
          }
          catch (DivideByZeroException)
          {
-            // Перехватываем исключение.
-            Console.WriteLine("Исключение перехвачено.");
+            // Перехватываем исключение
+            Console.WriteLine("Исключение перехвачено");
          }
-         Console.WriteLine("После try/catch-блока.");
+         Console.WriteLine("После try/catch-блока");
 
          Console.ReadKey();
       }
