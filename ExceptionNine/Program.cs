7c2ed639fe131be1bb7c9a8824aeeb449461d7e5
zenin5479 <zenin5479@gmail.com>
2024-09-06 22:57:48 +0300

    Update 5.6 Обновлен метод

---
+++ ExceptionNine/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Повторное генерирование исключения
+// System.Exception
+
+namespace ExceptionNine
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         // Здесь массив numer длиннее массива denom.
+         int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+         int[] denom = { 2, 0, 4, 4, 0, 8 };
+         for (int i = 0; i < numer.Length; i++)
+         {
+            try
+            {
+               Console.WriteLine(numer[i] + " / " +
+                                 denom[i] + " равно " + numer[i] / denom[i]);
+            }
+            catch (DivideByZeroException)
+            {
+               // Перехватываем исключение.
+               Console.WriteLine("Делить на нуль нельзя!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+               // Перехватываем исключение.
+               Console.WriteLine(
+                  "Нет соответствующего элемента.");
+               throw; // Генерируем исключение повторно.
+            }
+         }
+      }
+
+
+   }
+}
