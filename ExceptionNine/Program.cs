4e66dd8e5209408e8a22b81d93badfb03fb3df8f
zenin5479 <zenin5479@gmail.com>
2024-09-06 22:59:30 +0300

    Update 5.7 Обновлен метод

--- ExceptionNine/Program.cs
+++ ExceptionNine/Program.cs
@@ -9,31 +9,47 @@ namespace ExceptionNine
    {
       static void Main(string[] args)
       {
-         // Здесь массив numer длиннее массива denom.
-         int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
-         int[] denom = { 2, 0, 4, 4, 0, 8 };
-         for (int i = 0; i < numer.Length; i++)
+         try
          {
-            try
-            {
-               Console.WriteLine(numer[i] + " / " +
-                                 denom[i] + " равно " + numer[i] / denom[i]);
-            }
-            catch (DivideByZeroException)
-            {
-               // Перехватываем исключение.
-               Console.WriteLine("Делить на нуль нельзя!");
-            }
-            catch (IndexOutOfRangeException)
-            {
-               // Перехватываем исключение.
-               Console.WriteLine(
-                  "Нет соответствующего элемента.");
-               throw; // Генерируем исключение повторно.
-            }
+            Rethrow.genException();
+         }
+         catch (IndexOutOfRangeException)
+         {
+            // Перехватываем повторно
+            // сгенерированное исключение.
+            Console.WriteLine("Неисправимая ошибка — " +
+                              "программа завершена.");
          }
       }
 
-
+      class Rethrow
+      {
+         public static void genException()
+         {
+            // Здесь массив numer длиннее массива denom.
+            int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+            int[] denom = { 2, 0, 4, 4, 0, 8 };
+            for (int i = 0; i < numer.Length; i++)
+            {
+               try
+               {
+                  Console.WriteLine(numer[i] + " / " +
+                                    denom[i] + " равно " + numer[i] / denom[i]);
+               }
+               catch (DivideByZeroException)
+               {
+                  // Перехватываем исключение.
+                  Console.WriteLine("Делить на нуль нельзя!");
+               }
+               catch (IndexOutOfRangeException)
+               {
+                  // Перехватываем исключение.
+                  Console.WriteLine(
+                     "Нет соответствующего элемента.");
+                  throw; // Генерируем исключение повторно.
+               }
+            }
+         }
+      }
    }
 }
