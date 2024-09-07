02ab15ed696e7f923f0c203aa6ae4c01c01ef377
zenin5479 <zenin5479@gmail.com>
2024-09-07 21:51:26 +0300

    Update 7.6 Обновлен метод

--- ExceptionEleven/Program.cs
+++ ExceptionEleven/Program.cs
@@ -14,16 +14,14 @@ namespace ExceptionEleven
          {
             // Перехватываем исключение.
             Console.WriteLine("Стандартное сообщение таково: ");
-            Console.WriteLine(exc); // Вызов метода ToString().
-            Console.WriteLine("Свойство StackTrace: " +
-                              exc.StackTrace);
-            Console.WriteLine("Свойство Message: " +
-                              exc.Message);
-            Console.WriteLine("Свойство TargetSite: " +
-                              exc.TargetSite);
+            // Вызов метода ToString()
+            Console.WriteLine(exc); 
+            Console.WriteLine("Свойство StackTrace: " + exc.StackTrace);
+            Console.WriteLine("Свойство Message: " + exc.Message);
+            Console.WriteLine("Свойство TargetSite: " + exc.TargetSite);
          }
-         Console.WriteLine("После catch-инструкции.");
 
+         Console.WriteLine("После catch-инструкции");
          Console.ReadKey();
       }
    }
@@ -34,14 +32,13 @@ namespace ExceptionEleven
       {
          int[] nums = new int[4];
          Console.WriteLine("Перед генерированием исключения.");
-         // Генерируем исключение, связанное с попаданием
-         // индекса вне диапазона.
+         // Генерируем исключение, связанное с попаданием индекса вне диапазона
          for (int i = 0; i < 10; i++)
          {
             nums[i] = i;
             Console.WriteLine("nums[{0}]: {1}", i, nums[i]);
          }
-         Console.WriteLine("Этот текст не отображается.");
+         Console.WriteLine("Этот текст не отображается");
       }
    }
 }
