0fe4b2808386c8a6f19b4348eb62719feeca77d0
zenin5479 <zenin5479@gmail.com>
2024-09-07 21:59:33 +0300

    Update 8.0 Обновлен метод

---
+++ ExceptionTwelve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionTwelve
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
