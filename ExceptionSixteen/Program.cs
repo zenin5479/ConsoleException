fb84a8ad1c16c2ca08e94e8819c2a0091ff208e0
zenin5479 <zenin5479@gmail.com>
2024-09-08 11:36:51 +0300

    Update 10.4 Обновлен метод

--- ExceptionSixteen/Program.cs
+++ ExceptionSixteen/Program.cs
@@ -7,9 +7,46 @@ namespace ExceptionSixteen
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
-         Console.WriteLine("Hello World!");
+         byte a, b;
+         byte result;
+         a = 127;
+         b = 127;
+         try
+         {
+            unchecked
+            {
+               a = 127;
+               b = 127;
+               result = unchecked((byte)(a * b));
+               Console.WriteLine("Unchecked-результат: " + result);
+               a = 125;
+               b = 5;
+               result = unchecked((byte)(a * b));
+               Console.WriteLine("Unchecked-реэультат: " + result);
+            }
+            checked
+            {
+               a = 2;
+               b = 7;
+               result = checked((byte)(a * b)); // Все в порядке.
+               Console.WriteLine("Checked-результат: " + result);
+               a = 127;
+               b = 127;
+               result = checked((byte)(a * b)); // Здесь должно
+               // быть сгенерировано
+               // исключение.
+               Console.WriteLine("Checked-результат: " +
+                                 result); // Эта инструкция не
+               // выполнится.
+            }
+         }
+         catch (OverflowException exc)
+         {
+            // Перехватываем исключение.
+            Console.WriteLine(exc);
+         }
 
          Console.ReadKey();
       }
