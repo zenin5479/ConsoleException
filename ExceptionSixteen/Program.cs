06dde94a8fde6fecf68b6ec4b4e23c39995bf335
zenin5479 <zenin5479@gmail.com>
2024-09-08 11:36:03 +0300

    Update 10.3 Обновлен метод

---
+++ ExceptionSixteen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Использование ключевых слов checked и unchecked для блоков инструкций
+// System.Exception
+
+namespace ExceptionSixteen
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+
+         Console.ReadKey();
+      }
+   }
+}
