79a41efe3a415ec3aeb1a081ac92b842b92f2ff6
zenin5479 <zenin5479@gmail.com>
2024-09-08 11:20:30 +0300

    Update 9.6 Обновлен метод

---
+++ ExceptionFourteen/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+// Инструкции перехвата исключений производных классов
+// должны стоять перед инструкциями перехвата исключений базовых классов
+// System.Exception
+
+namespace ExceptionFourteen
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         for (int x = 0; x < 3; x++)
+         {
+            try
+            {
+               if (x == 0) throw new ExceptA(
+                  "Перехват исключения типа ExceptA.");
+               else if (x == 1) throw new ExceptB(
+                  "Перехват исключения типа ExceptB.");
+               else throw new Exception();
+            }
+            catch (ExceptB exc)
+            { // Перехватываем исключение.
+               Console.WriteLine(exc);
+            }
+            catch (ExceptA exc)
+            { // Перехватываем исключение.
+               Console.WriteLine(exc);
+            }
+            catch (Exception exc)
+            {
+               Console.WriteLine(exc);
+            }
+         }
+
+         Console.ReadKey();
+      }
+   }
+
+   // Создаем класс исключения.
+   class ExceptA : ApplicationException
+   {
+      public ExceptA() : base() { }
+      public ExceptA(string str) : base(str) { }
+      public override string ToString()
+      {
+         return Message;
+      }
+   }
+   // Создаем класс исключения как производный
+   //от класса ExceptA.
+   class ExceptB : ExceptA
+   {
+      public ExceptB() : base() { }
+      public ExceptB(string str) : base(str) { }
+      public override string ToString()
+      {
+         return Message;
+      }
+   }
+}
