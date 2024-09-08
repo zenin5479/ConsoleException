1339e3eaddcc4c698d8f292ab6c407e2639abb7c
zenin5479 <zenin5479@gmail.com>
2024-09-08 11:29:45 +0300

    Update 10.0 Обновлен метод

--- ExceptionFifteen/Program.cs
+++ ExceptionFifteen/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+// Использование ключевых слов checked и unchecked
+// System.Exception
+
 namespace ExceptionFifteen
 {
    internal class Program
    {
       static void Main()
       {
-         Console.WriteLine("Hello World!");
+         byte a, b;
+         byte result;
+         a = 127;
+         b = 127;
+         try
+         {
+            result = unchecked((byte)(a * b));
+            Console.WriteLine("Unchecked-результат: " + result);
+            result = checked((byte)(a * b)); // Эта инструкция
+            // вызывает исключение.
+            Console.WriteLine("Checked-реэультат: " +
+                              result); // Инструкция не будет
+            // выполнена.
+         }
+         catch (OverflowException exc)
+         {
+            // Перехватываем исключение.
+            Console.WriteLine(exc);
+         }
 
          Console.ReadKey();
       }
