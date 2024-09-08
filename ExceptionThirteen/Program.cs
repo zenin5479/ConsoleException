2b2e4e16e97be82c4bc728af98516b8fc6c9ff71
zenin5479 <zenin5479@gmail.com>
2024-09-08 10:32:22 +0300

    Update 8.7 Обновлен метод

---
+++ ExceptionThirteen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Использование исключения типа NullReferenceException
+// System.Exception
+
+namespace ExceptionThirteen
+{
+   internal class Program
+   {
+      static void Main()
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
