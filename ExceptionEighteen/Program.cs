280787a822499ac8af7497e2a33b3f93cc908cf4
zenin5479 <zenin5479@gmail.com>
2024-09-15 21:19:55 +0300

    Update 11.0 Обновлен метод

--- ExceptionEighteen/Program.cs
+++ ExceptionEighteen/Program.cs
@@ -11,38 +11,17 @@ namespace ExceptionEighteen
    {
       static void Main()
       {
-         HandlingExceptions();
          Console.WriteLine(MakeRequest().Result);
          Console.ReadKey();
       }
 
-      public static void HandlingExceptions()
+      private static async Task<string> MakeRequest()
       {
-         int x = 1;
-         int y = 0;
-
+         HttpClient client = new HttpClient();
+         Task<string> streamTask = client.GetStringAsync("https://metanit.com");
          try
          {
-            int result1 = x / y;
-            int result2 = y / x;
-         }
-         catch (DivideByZeroException) when (y == 0)
-         {
-            Console.WriteLine("y не должен быть равен 0");
-         }
-         catch (DivideByZeroException ex)
-         {
-            Console.WriteLine(ex.Message);
-         }
-      }
-
-      public static async Task<string> MakeRequest()
-      {
-         var client = new HttpClient();
-         var streamTask = client.GetStringAsync("https://metanit.com");
-         try
-         {
-            var responseText = await streamTask;
+            string responseText = await streamTask;
             return responseText;
          }
          catch (HttpRequestException e) when (e.Message.Contains("301"))
